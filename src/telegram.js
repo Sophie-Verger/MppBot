accba7543d8304564bb035cfd5ce60ae0e4b6f35
Dinergo <58221330+Dinergo@users.noreply.github.com>
2022-06-08 12:00:06 +0200

    Correction de bug générés par erreur

--- src/telegram.js
+++ src/telegram.js
@@ -4,14 +4,12 @@ const Composer = require('telegraf');
 const WizardScene = require('telegraf');
 const Stage = require('telegraf');
 
-
 // Création de la scène du daily
 
 const dailyWizard = new Scenes.WizardScene(
     'DAILY_ID', // first argument is Scene_ID, 
     (ctx) => {
         ctx.reply("Début du daily");
-
         ctx.reply("Qu'as-tu fais dans ton précédent jour de travail ?");
         ctx.wizard.state.daily = {};
         return ctx.wizard.next();
@@ -32,28 +30,28 @@ const dailyWizard = new Scenes.WizardScene(
         ctx.wizard.state.daily.annoyances = ctx.message.text;
         console.log(ctx.wizard.state);
         ctx.reply(ctx.wizard.state.daily);
-        // renvoyer les infos quelque part en extérieur. Un canal, ou 
+        // renvoyer les infos quelque part en extérieur. Un autre canal, un mail, etc.
         return ctx.scene.leave();
     },
-  );
+);
 
 const stage = new Scenes.Stage([dailyWizard]);
 
-///// initialisation du bot
 
+///// Initialisation du bot
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+bot.use(session());
 bot.use(stage.middleware());
 
-/// commandes
+
+/// Ajout des commandes
 bot.command('daily', (ctx) => {
     ctx.scene.enter('DAILY_ID');
 });
 
-
 bot.hears('test', (ctx) => {
     ctx.reply("C'est ok pour moi");
 })
-
 
 bot.command('chat', (ctx) => {
     let convId = ctx.message.from.id; // est l'ID de la personne, du chat sinon
