590105625a7f999ddb835989e8ff3b9ffc4d443f
ophélie <ophelieleleu@yahoo.fr>
2022-06-09 11:22:27 +0200

    code commenté modification secu discord

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,9 +1,11 @@
+// for use Slash command
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 
+// Slash command
 const commands = [
 	new SlashCommandBuilder().setName('daily').setDescription("C'est le moment !"),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
@@ -11,6 +13,7 @@ const commands = [
 ];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+//for push in commands Not use deploy-commands
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+// Require the necessary discord.js classes
 const Discord = require('discord.js');
 const { MessageActionRow, Modal, TextInputComponent } = require('discord.js');
 const {token} = require('./config.json');
+// Create a new client instance
 const client = new Discord.Client({intents: 32767});
 
-
+// When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+// Listen for interactions Slash
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
@@ -21,19 +24,24 @@ client.on('interactionCreate', async interaction => {
 
 });
 
+// Modal Daily
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
 	if (interaction.commandName === 'daily') {
-	
+		
+		// Create the modal
 		const modal = new Modal()
 			.setCustomId('myModal')
 			.setTitle(`Bonjour ${interaction.user.tag} prête à commencer le daily ?`)
-			//.setTitle("daily")
 		
+		// Add components to modal
+		// Create the text input components
 		const questionOne = new TextInputComponent()
 			.setCustomId('questionOne')
+			// The label is the prompt the user sees for this input
 			.setLabel("Qu'as-tu fais hier ?")
+			// Paragraph means multiple lines of text.
 			.setStyle('PARAGRAPH');
 
 		const questionTwo = new TextInputComponent()
@@ -42,11 +50,15 @@ client.on('interactionCreate', async interaction => {
 			.setStyle('PARAGRAPH');
 
 
+		// An action row only holds one text input,
+		// so you need one action row per text input.
 		const firstActionRow = new MessageActionRow().addComponents(questionOne);
 		const secondActionRow = new MessageActionRow().addComponents(questionTwo);
 
+		// Add inputs to the modal
 		modal.addComponents(firstActionRow, secondActionRow);
 
+		// Show the modal
 		await interaction.showModal(modal);
 	}
 });
@@ -54,12 +66,14 @@ client.on('interactionCreate', async interaction => {
 client.on('interactionCreate', interaction => {
 	if (!interaction.isModalSubmit()) return;
 
+	// Get the data entered by the user
 	const questionYesterday = interaction.fields.getTextInputValue('questionOne');
 	const questionToday = interaction.fields.getTextInputValue('questionTwo');
 
 	console.log({ questionYesterday, questionToday});
 });
 
+// Login to Discord with your client's token
 client.login(token);
  
 
